73aa76fc0b6919884d3167e67ffbb8ac028d0768
Alcir Jr <alcir_jr13@hotmail.com>
2021-05-11 11:28:08 -0300

    JavaScript Objetos

---
+++ JavaScript/index.js
@@ -0,0 +1,29 @@
+class Cliente {
+  nome;
+  cpf;
+  rg;
+  agencia;
+  saldo;
+}
+
+const cliente1 = new Cliente();
+const cliente2 = new Cliente();
+const cliente3 = new Cliente();
+
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233309;
+cliente1.rg = 123456789;
+cliente1.agencia = 1001;
+cliente1.saldo = 0;
+
+cliente2.nome = "Alice";
+cliente2.cpf = 44455566608;
+cliente2.rg = 456789123;
+cliente2.agencia = 1001;
+cliente2.saldo = 0;
+
+cliente3.nome = "Bete";
+cliente3.cpf = 77788869909;
+cliente3.rg = 987654321;
+cliente3.agencia = 1001;
+cliente3.saldo = 0;
