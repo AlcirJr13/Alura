02f64f940a0de3c4838e1c55fc66c937af59cd5e
Alcir Jr <alcir_jr13@hotmail.com>
2021-07-09 18:50:18 -0300

    Customizando mensagens de erro - Nome, email, senha e adta de nascimento

--- Validação de Formulários e HTML5/js/validacao.js
+++ Validação de Formulários e HTML5/js/validacao.js
@@ -4,11 +4,58 @@ export function valida(input) {
   if (validadores[tipoDeInput]) {
     validadores[tipoDeInput](input);
   }
+
+  if (!input.validity.valid) {
+    input.parentElement.classList.add("input-container--invalido");
+    input.parentElement.querySelector(".input-mensagem-erro").innerHTML =
+      mostraMensagemDeErro(tipoDeInput, input);
+  } else {
+    input.parentElement.classList.remove("input-container--invalido");
+    input.parentElement.querySelector(".input-mensagem-erro").innerHTML = "";
+  }
 }
+
+const tiposDeErro = [
+  "valueMissing",
+  "typeMismatch",
+  "patternMismatch",
+  "customError",
+];
+
+const mensagensDeErros = {
+  nome: {
+    valueMissing: "O campo Nome não pode estar vazio.",
+  },
+  email: {
+    valueMissing: "O campo Email não pode estar vazio.",
+    typeMismatch: "digite um email válido",
+  },
+  senha: {
+    valueMissing: "O campo Senha não pode estar vazio.",
+    patternMismatch:
+      "A senha deve conter entre 4 a 20 caracteres, com pelo menos 1 letra maíuscula, 1 letra minúscula, 1 número e 1 símbolo.",
+  },
+  dataNascimento: {
+    valueMissing: "O campo de data de nascimento não pode estar vazio.",
+    customError: "Você deve ser maior de idade para se cadastrar.",
+  },
+};
 
 const validadores = {
   dataNascimento: (input) => validadaDataNascimento(input),
 };
+
+function mostraMensagemDeErro(tipoDeInput, input) {
+  let mensagem = "";
+
+  tiposDeErro.forEach((erro) => {
+    if (input.validity[erro]) {
+      mensagem = mensagensDeErros[tipoDeInput][erro];
+    }
+  });
+
+  return mensagem;
+}
 
 function validadaDataNascimento(input) {
   const dataRecebida = new Date(input.value);
