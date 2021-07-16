6a4ab54c3115cc55472551062c2e1f8481763baf
Alcir Jr <alcir_jr13@hotmail.com>
2021-07-16 18:08:21 -0300

    Calculando imc - modificando DOM - pqna modificação no css para classe invalida

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,1 +1,36 @@
-console.log("ola");
+var pacientes = document.querySelectorAll(".paciente");
+console.log(pacientes);
+
+for (var i = 0; i < pacientes.length; i++) {
+  var paciente = pacientes[i];
+
+  var tdPeso = paciente.querySelector(".info-peso");
+  var peso = tdPeso.textContent;
+
+  var tdAltura = paciente.querySelector(".info-altura");
+  var altura = tdAltura.textContent;
+
+  var tdImc = paciente.querySelector(".info-imc");
+
+  var pesoValido = true;
+  var alturaValido = true;
+
+  if (peso <= 0 || peso >= 450) {
+    console.log("Peso inválido");
+    var pesoValido = false;
+    tdImc.textContent = "Peso inválido";
+    paciente.classList.add("paciente-invalido");
+  }
+
+  if (altura <= 0 || altura >= 3) {
+    console.log("Altura inválido");
+    var alturaValido = false;
+    tdImc.textContent = "Altura inválido";
+    paciente.classList.add("paciente-invalido");
+  }
+
+  if (alturaValido && pesoValido) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc.toFixed(2);
+  }
+}
