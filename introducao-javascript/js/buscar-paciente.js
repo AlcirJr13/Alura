ee8928ead83779785d3fa27655df8332796f4a3c
Alcir Jr <alcir_jr13@hotmail.com>
2021-08-03 11:45:39 -0300

    Corrigindo erros

--- introducao-javascript/js/buscar-paciente.js
+++ introducao-javascript/js/buscar-paciente.js
@@ -3,13 +3,20 @@ var botaoAdicionar = document.querySelector("#buscar-paciente");
 botaoAdicionar.addEventListener("click", () => {
   var xhr = new XMLHttpRequest();
   xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
-  xhr.addEventListener("load", () => {
-    var resposta = xhr.responseText;
-    var pacientes = JSON.parse(resposta);
 
-    pacientes.forEach((paciente) => {
-      adicionaPacienteNaTabela(paciente);
-    });
+  xhr.addEventListener("load", () => {
+    var erroAjax = document.querySelector("#erro-ajax");
+    erroAjax.classList.add("invisivel");
+    if (xhr.status == 200) {
+      var resposta = xhr.responseText;
+      var pacientes = JSON.parse(resposta);
+
+      pacientes.forEach((paciente) => {
+        adicionaPacienteNaTabela(paciente);
+      });
+    } else {
+      erroAjax.classList.remove("invisivel");
+    }
   });
   xhr.send();
 });
