20a5629edf8527e6ecb91d973f256cfa7fffdcb4
Alcir Jr <alcir_jr13@hotmail.com>
2021-08-31 20:55:28 -0300

    propriedades

--- React - Entendendo como a biblioteca funciona/ceep/src/App.js
+++ React - Entendendo como a biblioteca funciona/ceep/src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
-import ListaDeNotas from "./components/ListaDeNotas";
-import FormularioCadastro from "./components/FormularioCadastro";
 import "./assets/App.css";
-import './assets/index.css';
+import "./assets/index.css";
+import FormularioCadastro from "./components/FormularioCadastro";
+import ListaDeNotas from "./components/ListaDeNotas";
 class App extends Component {
+  criarNota(titulo, texto) {
+    console.log(`Uma nota foi criada` + titulo + " " + titulo);
+  }
+
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro />
+        <FormularioCadastro criarNota={this.criarNota} />
         <ListaDeNotas />
       </section>
     );
