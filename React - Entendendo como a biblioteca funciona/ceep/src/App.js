a91c3c7e372556b0e1225f51dc0eb62a10b512c2
Alcir Jr <alcir_jr13@hotmail.com>
2021-09-25 16:03:56 -0300

    Finalizando projeto

--- React - Entendendo como a biblioteca funciona/ceep/src/App.js
+++ React - Entendendo como a biblioteca funciona/ceep/src/App.js
@@ -1,21 +1,36 @@
 import React, { Component } from "react";
-import "./assets/App.css";
-import "./assets/index.css";
-import FormularioCadastro from "./components/FormularioCadastro";
 import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import './assets/index.css';
 class App extends Component {
-  criarNota(titulo, texto) {
-    console.log(`Uma nota foi criada` + titulo + " " + titulo);
+
+  constructor(){
+    super();
+
+    this.state = {
+      notas:[]
+    }
+  }
+
+  criarNota(titulo, texto){
+    const novaNota = {titulo, texto};
+    const novoArrayNotas = [...this.state.notas,novaNota]
+    const novoEstado = {
+      notas:novoArrayNotas
+    }
+    this.setState(novoEstado)
   }
 
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota={this.criarNota} />
-        <ListaDeNotas />
+        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <ListaDeNotas notas={this.state.notas}/>
       </section>
     );
   }
 }
 
+//new ListaDeNotas({notas:this.notas})
 export default App;
