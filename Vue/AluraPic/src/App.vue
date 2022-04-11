3df918381522e0106bb39de4c15491858110b5df
Alcir Jr <alcir_jr13@hotmail.com>
2022-04-11 17:45:32 -0300

    Modificações na estrutura

--- Vue/AluraPic/src/App.vue
+++ Vue/AluraPic/src/App.vue
@@ -4,7 +4,12 @@
 
     <ul class="lista-fotos">
       <li class="lista-fotos-item" v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.titulo" />
+        <div class="painel">
+          <h2 class="painel-titulo">{{ foto.titulo }}</h2>
+          <div class="painel-conteudo">
+            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
+          </div>
+        </div>
       </li>
     </ul>
   </div>
@@ -15,34 +20,34 @@ export default {
   data() {
     return {
       titulo: "AluraPic",
-      fotos: []
+      fotos: [],
     };
   },
   created() {
     let promise = this.$http.get("http://localhost:3000/v1/fotos");
     promise
-      .then(res => res.json())
+      .then((res) => res.json())
       .then(
-        fotos => (this.fotos = fotos),
-        err => console.log(err)
+        (fotos) => (this.fotos = fotos),
+        (err) => console.log(err)
       );
 
     //this.$this.get("http://localhost:3000/v1/fotos");
     //.then(res=>ers.json())
     //.then(fotos=>this.fotos = fotos)
-  }
+  },
 };
 </script>
 
 <style>
-.corpo {
-  font-family: Helvetica, sans-serif;
-  width: 96%;
-  margin: 0 auto;
-}
-
 .centralizado {
   text-align: center;
+}
+
+.corpo {
+  font-family: Helvetica, sans-serif;
+  margin: 0 auto;
+  width: 96%;
 }
 
 .lista-fotos {
@@ -51,5 +56,32 @@ export default {
 
 .lista-fotos .lista-fotos-item {
   display: inline-block;
+}
+
+.imagem-responsiva {
+  width: 100%;
+}
+
+/* estilo do painel */
+
+.painel {
+  padding: 0 auto;
+  border: solid 2px grey;
+  display: inline-block;
+  margin: 5px;
+  box-shadow: 5px 5px 10px grey;
+  width: 200px;
+  height: 100%;
+  vertical-align: top;
+  text-align: center;
+}
+
+.painel .painel-titulo {
+  text-align: center;
+  border: solid 2px;
+  background: lightblue;
+  margin: 0 0 15px 0;
+  padding: 10px;
+  text-transform: uppercase;
 }
 </style>
