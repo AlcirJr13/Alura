a01e3f02bc942bd88884ffe9a8a2b5299be23eb6
Alcir Jr <alcir_jr13@hotmail.com>
2022-04-13 19:31:55 -0300

    Criando filtro

--- Vue/AluraPic/src/App.vue
+++ Vue/AluraPic/src/App.vue
@@ -2,8 +2,15 @@
   <div class="corpo">
     <h1 class="centralizado">{{ titulo }}</h1>
 
+    <input
+      type="search"
+      class="filtro"
+      placeholder="Filtre por parte do título."
+      v-on:input="filtro = $event.target.value"
+    />
+
     <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotos">
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
         <meu-painel :titulo="foto.titulo">
           <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
         </meu-painel>
@@ -23,7 +30,19 @@ export default {
     return {
       titulo: "AluraPic",
       fotos: [],
+      filtro: "",
     };
+  },
+
+  computed: {
+    fotosComFiltro() {
+      if (this.filtro) {
+        let exp = new RegExp(this.filtro.trim(), "i");
+        return this.fotos.filter((foto) => exp.test(foto.titulo));
+      } else {
+        return this.fotos;
+      }
+    },
   },
   created() {
     let promise = this.$http.get("http://localhost:3000/v1/fotos");
@@ -61,6 +80,11 @@ export default {
 }
 
 .imagem-responsiva {
+  width: 100%;
+}
+
+.filtro {
+  display: block;
   width: 100%;
 }
 </style>
