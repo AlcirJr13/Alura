00c1b306bc5fe5f538f1b0edb3ef4da7113bb912
Alcir - Pc da Câmara <alcir_jr13@hotmail.com>
2022-08-30 13:17:15 -0300

    nuxt Free inicio

---
+++ nuxtFree/nuxtFree/pages/servicos/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et tenetur
+      inventore nobis veritatis aperiam esse, fuga nam ullam quibusdam, eaque
+      adipisci facere magnam. Ratione magni tempore corporis hic impedit unde!
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data() {
+    return {}
+  },
+  methods: {},
+}
+</script>
+
+<style></style>
